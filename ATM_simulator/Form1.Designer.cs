5ea2543389fb59c62bad8d399daa7af0b5413016
CaramelYo_CGVSL_PC <alltime326@gmail.com>
2018-09-30 23:36:12 +0800

    first commit

---
+++ ATM_simulator/Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace ATM_simulator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.login_textbox = new System.Windows.Forms.TextBox();
+            this.login_btn = new System.Windows.Forms.Button();
+            this.login_label = new System.Windows.Forms.Label();
+            this.service_title = new System.Windows.Forms.Label();
+            this.withdraw_btn = new System.Windows.Forms.Button();
+            this.top_up_btn = new System.Windows.Forms.Button();
+            this.check_balance_btn = new System.Windows.Forms.Button();
+            this.check_history_btn = new System.Windows.Forms.Button();
+            this.logout_btn = new System.Windows.Forms.Button();
+            this.money_label = new System.Windows.Forms.Label();
+            this.history_textbox = new System.Windows.Forms.TextBox();
+            this.NTD_btn = new System.Windows.Forms.Button();
+            this.USD_btn = new System.Windows.Forms.Button();
+            this.money_textbox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // login_textbox
+            // 
+            this.login_textbox.Location = new System.Drawing.Point(451, 318);
+            this.login_textbox.Name = "login_textbox";
+            this.login_textbox.PasswordChar = '*';
+            this.login_textbox.Size = new System.Drawing.Size(100, 22);
+            this.login_textbox.TabIndex = 0;
+            // 
+            // login_btn
+            // 
+            this.login_btn.Location = new System.Drawing.Point(573, 318);
+            this.login_btn.Name = "login_btn";
+            this.login_btn.Size = new System.Drawing.Size(75, 23);
+            this.login_btn.TabIndex = 1;
+            this.login_btn.Text = "登入";
+            this.login_btn.UseVisualStyleBackColor = true;
+            this.login_btn.Click += new System.EventHandler(this.login_btn_Click);
+            // 
+            // login_label
+            // 
+            this.login_label.AutoSize = true;
+            this.login_label.Font = new System.Drawing.Font("PMingLiU", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.login_label.Location = new System.Drawing.Point(291, 316);
+            this.login_label.Name = "login_label";
+            this.login_label.Size = new System.Drawing.Size(154, 24);
+            this.login_label.TabIndex = 2;
+            this.login_label.Text = "請輸入6~12為晶片卡密碼:\r\nEnter PIN (6~12 Digits)";
+            // 
+            // service_title
+            // 
+            this.service_title.AutoSize = true;
+            this.service_title.Font = new System.Drawing.Font("PMingLiU", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.service_title.Location = new System.Drawing.Point(415, 271);
+            this.service_title.Name = "service_title";
+            this.service_title.Size = new System.Drawing.Size(142, 19);
+            this.service_title.TabIndex = 3;
+            this.service_title.Text = "請選擇服務項目";
+            // 
+            // withdraw_btn
+            // 
+            this.withdraw_btn.Location = new System.Drawing.Point(419, 309);
+            this.withdraw_btn.Name = "withdraw_btn";
+            this.withdraw_btn.Size = new System.Drawing.Size(138, 27);
+            this.withdraw_btn.TabIndex = 4;
+            this.withdraw_btn.Text = "存簿提款";
+            this.withdraw_btn.UseVisualStyleBackColor = true;
+            this.withdraw_btn.Click += new System.EventHandler(this.withdraw_btn_Click);
+            // 
+            // top_up_btn
+            // 
+            this.top_up_btn.Location = new System.Drawing.Point(419, 343);
+            this.top_up_btn.Name = "top_up_btn";
+            this.top_up_btn.Size = new System.Drawing.Size(138, 27);
+            this.top_up_btn.TabIndex = 5;
+            this.top_up_btn.Text = "存簿存款";
+            this.top_up_btn.UseVisualStyleBackColor = true;
+            this.top_up_btn.Click += new System.EventHandler(this.top_up_btn_Click);
+            // 
+            // check_balance_btn
+            // 
+            this.check_balance_btn.Location = new System.Drawing.Point(419, 377);
+            this.check_balance_btn.Name = "check_balance_btn";
+            this.check_balance_btn.Size = new System.Drawing.Size(138, 27);
+            this.check_balance_btn.TabIndex = 6;
+            this.check_balance_btn.Text = "查詢餘額";
+            this.check_balance_btn.UseVisualStyleBackColor = true;
+            this.check_balance_btn.Click += new System.EventHandler(this.check_balance_btn_Click);
+            // 
+            // check_history_btn
+            // 
+            this.check_history_btn.Location = new System.Drawing.Point(419, 410);
+            this.check_history_btn.Name = "check_history_btn";
+            this.check_history_btn.Size = new System.Drawing.Size(138, 27);
+            this.check_history_btn.TabIndex = 7;
+            this.check_history_btn.Text = "查詢歷史紀錄";
+            this.check_history_btn.UseVisualStyleBackColor = true;
+            this.check_history_btn.Click += new System.EventHandler(this.check_history_btn_Click);
+            // 
+            // logout_btn
+            // 
+            this.logout_btn.Location = new System.Drawing.Point(419, 443);
+            this.logout_btn.Name = "logout_btn";
+            this.logout_btn.Size = new System.Drawing.Size(138, 27);
+            this.logout_btn.TabIndex = 8;
+            this.logout_btn.Text = "登出";
+            this.logout_btn.UseVisualStyleBackColor = true;
+            this.logout_btn.Click += new System.EventHandler(this.logout_btn_Click);
+            // 
+            // money_label
+            // 
+            this.money_label.AutoSize = true;
+            this.money_label.Font = new System.Drawing.Font("PMingLiU", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.money_label.Location = new System.Drawing.Point(291, 373);
+            this.money_label.Name = "money_label";
+            this.money_label.Size = new System.Drawing.Size(95, 12);
+            this.money_label.TabIndex = 9;
+            this.money_label.Text = "請輸入提款金額: ";
+            // 
+            // history_textbox
+            // 
+            this.history_textbox.Enabled = false;
+            this.history_textbox.Location = new System.Drawing.Point(302, 255);
+            this.history_textbox.Multiline = true;
+            this.history_textbox.Name = "history_textbox";
+            this.history_textbox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.history_textbox.Size = new System.Drawing.Size(401, 182);
+            this.history_textbox.TabIndex = 10;
+            // 
+            // NTD_btn
+            // 
+            this.NTD_btn.Location = new System.Drawing.Point(573, 347);
+            this.NTD_btn.Name = "NTD_btn";
+            this.NTD_btn.Size = new System.Drawing.Size(75, 23);
+            this.NTD_btn.TabIndex = 11;
+            this.NTD_btn.Text = "新台幣";
+            this.NTD_btn.UseVisualStyleBackColor = true;
+            this.NTD_btn.Click += new System.EventHandler(this.NTD_btn_Click);
+            // 
+            // USD_btn
+            // 
+            this.USD_btn.Location = new System.Drawing.Point(573, 381);
+            this.USD_btn.Name = "USD_btn";
+            this.USD_btn.Size = new System.Drawing.Size(75, 23);
+            this.USD_btn.TabIndex = 12;
+            this.USD_btn.Text = "美金";
+            this.USD_btn.UseVisualStyleBackColor = true;
+            this.USD_btn.Click += new System.EventHandler(this.USD_btn_Click);
+            // 
+            // money_textbox
+            // 
+            this.money_textbox.Location = new System.Drawing.Point(451, 363);
+            this.money_textbox.Name = "money_textbox";
+            this.money_textbox.Size = new System.Drawing.Size(100, 22);
+            this.money_textbox.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1006, 648);
+            this.Controls.Add(this.money_textbox);
+            this.Controls.Add(this.USD_btn);
+            this.Controls.Add(this.NTD_btn);
+            this.Controls.Add(this.history_textbox);
+            this.Controls.Add(this.money_label);
+            this.Controls.Add(this.logout_btn);
+            this.Controls.Add(this.check_history_btn);
+            this.Controls.Add(this.check_balance_btn);
+            this.Controls.Add(this.top_up_btn);
+            this.Controls.Add(this.withdraw_btn);
+            this.Controls.Add(this.service_title);
+            this.Controls.Add(this.login_label);
+            this.Controls.Add(this.login_btn);
+            this.Controls.Add(this.login_textbox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox login_textbox;
+        private System.Windows.Forms.Button login_btn;
+        private System.Windows.Forms.Label login_label;
+        private System.Windows.Forms.Label service_title;
+        private System.Windows.Forms.Button withdraw_btn;
+        private System.Windows.Forms.Button top_up_btn;
+        private System.Windows.Forms.Button check_balance_btn;
+        private System.Windows.Forms.Button check_history_btn;
+        private System.Windows.Forms.Button logout_btn;
+        private System.Windows.Forms.Label money_label;
+        private System.Windows.Forms.TextBox history_textbox;
+        private System.Windows.Forms.Button NTD_btn;
+        private System.Windows.Forms.Button USD_btn;
+        private System.Windows.Forms.TextBox money_textbox;
+    }
+}
+
---
+++ ATM_simulator/Form1.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ATM_simulator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            //// initialize control_list
+            //control_list = new List<List<Control>>();
+
+            //control_list.Add(new List<Control>() { login_label, login_textbox, login_btn });
+            //control_list.Add(new List<Control>() {
+            //    service_title, withdraw_btn, top_up_btn, check_balance_btn, check_history_btn, logout_btn
+            //});
+
+            control_list = new List<Control>() {
+                login_label, login_textbox, login_btn,
+                service_title, withdraw_btn, top_up_btn, check_balance_btn, check_history_btn, logout_btn,
+                money_label, money_textbox, NTD_btn, USD_btn,
+                history_textbox
+            };
+
+            // initialize status
+            status = 1;
+
+            // initialize money
+            money = 10000;
+            history = "[初始] " + money.ToString() + " NTD\r\n";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //foreach (List<Control> c_list in control_list)
+            //{
+            //    foreach (Control c in c_list)
+            //    {
+            //        c.Visible = false;
+            //    }
+            //}
+
+            //foreach (Control c in control_list[status])
+            //{
+            //    c.Visible = true;
+            //}
+
+            foreach (Control c in control_list)
+            {
+                c.Visible = false;
+            }
+            
+            switch (status)
+            {
+                case 0:
+                    // reserved
+                    break;
+                case 1:
+                    // login page
+                    login_label.Visible = true;
+                    login_textbox.Visible = true;
+                    login_btn.Visible = true;
+
+                    login_label.Text = "請輸入6~12為晶片卡密碼: \r\nEnter PIN (6~12 Digits)";
+                    login_textbox.Text = "";
+                    break;
+                case 2:
+                    // service page
+                    service_title.Visible = true;
+                    withdraw_btn.Visible = true;
+                    top_up_btn.Visible = true;
+                    check_balance_btn.Visible = true;
+                    check_history_btn.Visible = true;
+                    logout_btn.Visible = true;
+                    break;
+                case 3:
+                    // withdraw page
+                    money_label.Visible = true;
+                    money_textbox.Visible = true;
+                    NTD_btn.Visible = true;
+                    USD_btn.Visible = true;
+                    logout_btn.Visible = true;
+
+                    money_label.Text = "請輸入提款金額: ";
+                    money_textbox.Text = "";
+                    break;
+                case 4:
+                    // top_up page
+                    money_label.Visible = true;
+                    money_textbox.Visible = true;
+                    NTD_btn.Visible = true;
+                    USD_btn.Visible = true;
+                    logout_btn.Visible = true;
+
+                    money_label.Text = "請輸入存款金額: ";
+                    money_textbox.Text = "";
+                    break;
+                case 5:
+                    // check_balance page
+                    login_label.Visible = true;
+                    logout_btn.Visible = true;
+
+                    login_label.Text = "餘額剩下: " + money.ToString() + " NTD\r\n謝謝您的惠顧，歡迎下次再來！";
+                    break;
+                case 6:
+                    // check_history page
+                    history_textbox.Visible = true;
+                    logout_btn.Visible = true;
+
+                    history_textbox.Text = history;
+                    break;
+                default:
+                    // unexpected status
+                    history += "unexpected situation in status switch\r\n";
+                    break;
+            }
+        }
+        private void login_btn_Click(object sender, EventArgs e)
+        {
+            if (login_textbox.Text == "000000")
+            {
+                // correct password
+                status = 2;
+                Form1_Load(this, e);
+            }
+        }
+
+        private void withdraw_btn_Click(object sender, EventArgs e)
+        {
+            status = 3;
+            Form1_Load(this, e);
+        }
+
+        private void top_up_btn_Click(object sender, EventArgs e)
+        {
+            status = 4;
+            Form1_Load(this, e);
+        }
+
+        private void check_balance_btn_Click(object sender, EventArgs e)
+        {
+            status = 5;
+            Form1_Load(this, e);
+        }
+
+        private void check_history_btn_Click(object sender, EventArgs e)
+        {
+            status = 6;
+            Form1_Load(this, e);
+        }
+
+        private void logout_btn_Click(object sender, EventArgs e)
+        {
+            status = 1;
+            Form1_Load(this, e);
+        }
+
+        private void NTD_btn_Click(object sender, EventArgs e)
+        {
+            int NTD = 0;
+            try
+            {
+                NTD = int.Parse(money_textbox.Text);
+            }
+            catch
+            {
+                // unexpected situation
+                history += "unexpected situation in NTD_btn_Click\r\n";
+                return;
+            }
+
+            if (status == 3)
+            {
+                // withdraw status
+                if (money >= NTD)
+                {
+                    // sufficient
+                    money -= NTD;
+                    history += "[提款] " + NTD.ToString() + " NTD\r\n";
+                    history += "[餘額] " + money.ToString() + " NTD\r\n";
+                }
+            }
+            else if (status == 4)
+            {
+                // top_up status
+                money += NTD;
+                history += "[存款] " + NTD.ToString() + " NTD\r\n";
+                history += "[餘額] " + money.ToString() + " NTD\r\n";
+            }
+
+            status = 1;
+            Form1_Load(this, e);
+        }
+
+        private void USD_btn_Click(object sender, EventArgs e)
+        {
+            int USD = 0;
+            try
+            {
+                USD = int.Parse(money_textbox.Text);
+            }
+            catch
+            {
+                // unexpected situation
+                history += "unexpected situation in NTD_btn_Click\r\n";
+                return;
+            }
+
+            int NTD = USD * exchange_rate;
+
+            if (status == 3)
+            {
+                // withdraw status
+                if (money >= NTD)
+                {
+                    // sufficient
+                    money -= NTD;
+                    history += "[提款] " + NTD.ToString() + " NTD (" + USD.ToString() + " USD)\r\n";
+                    history += "[餘額] " + money.ToString() + " NTD\r\n";
+                }
+            }
+            else if (status == 4)
+            {
+                // top_up status
+                money += NTD;
+                history += "[存款] " + NTD.ToString() + " NTD (" + USD.ToString() + " USD)\r\n";
+                history += "[餘額] " + money.ToString() + " NTD\r\n";
+            }
+
+            status = 1;
+            Form1_Load(this, e);
+        }
+
+        //List<List<Control>> control_list;
+        List<Control> control_list;
+
+        string history;
+        int exchange_rate = 30;
+        int status, money;
+    }
+}
